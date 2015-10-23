4b687e901b9850919066e9d85a750cff7e16932b
sJhonny-e <sjhonny@inbox.com>
2015-10-23 18:25:41 +0100

    fixed some mistakes in tests data

--- test/primesCalculatorSpec.js
+++ test/primesCalculatorSpec.js
@@ -11,11 +11,11 @@ describe('primesCalculator', function(){
 
 		it('returns [2] when n =2', function(){
 			// using deep equal to assert that the array is ordered
-			expect(calculator.getPrimes(2)).to.deep.equal([2]);
+			expect(calculator.getPrimes(1)).to.deep.equal([2]);
 		});
 
 		it ('returns [2,3,5,7] when n = 4', function(){
-			expect(calculator.getPrimes(2)).to.deep.equal([2,3,5,7]);
+			expect(calculator.getPrimes(4)).to.deep.equal([2,3,5,7]);
 		});
 	});
 });
