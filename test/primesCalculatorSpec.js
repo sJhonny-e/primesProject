14970b66a8ff7dd5c4e46a4da38950c37aeec416
sJhonny-e <sjhonny@inbox.com>
2015-10-23 18:13:02 +0100

    primesCalculator silly implementation to pass 2nd test

--- test/primesCalculatorSpec.js
+++ test/primesCalculatorSpec.js
@@ -8,5 +8,9 @@ describe('primesCalculator', function(){
 		it('returns empty array when n < 2', function(){
 			expect(calculator.getPrimes(1)).to.be.empty;
 		});
+
+		it('returns [2] when n =2', function(){
+			expect(calculator.getPrimes(2)).to.have.members([2]);
+		});
 	});
 });
