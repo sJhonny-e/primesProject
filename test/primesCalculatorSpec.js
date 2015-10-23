84b39ea41294d6c7f8e7c15fafc1aeb91a4393e8
sJhonny-e <sjhonny@inbox.com>
2015-10-23 18:19:30 +0100

    fixed a test case- should return an empty array when n<1, not 2

--- test/primesCalculatorSpec.js
+++ test/primesCalculatorSpec.js
@@ -5,11 +5,12 @@ var calculator = require('../lib/primesCalculator');
 describe('primesCalculator', function(){
 
 	describe('#getPrimes(n)', function(){
-		it('returns empty array when n < 2', function(){
-			expect(calculator.getPrimes(1)).to.be.empty;
+		it('returns empty array when n < 1', function(){
+			expect(calculator.getPrimes(0)).to.be.empty;
 		});
 
 		it('returns [2] when n =2', function(){
+			// using deep equal to assert that the array is ordered
 			expect(calculator.getPrimes(2)).to.deep.equal([2]);
 		});
 
