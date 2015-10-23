a2b9775034b54768adb8ca33dd566c7ba637c93c
sJhonny-e <sjhonny@inbox.com>
2015-10-23 18:02:16 +0100

    first failing test for primesCalculator

---
+++ test/primesCalculatorSpec.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+
+var calculator = require('../lib/primesCalculator');
+
+describe('primesCalculator', function(){
+
+	describe('#getPrimes(n)', function(){
+		it('returns empty array when n < 2', function(){
+			expect(calculator.getPrimes(1)).to.be.empty();
+		});
+	});
+});
